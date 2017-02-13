4f29692cd9461ab86fd175595c91aa8fe8c7a367
zhaoran <2680131355@qq.com>
2017-02-13 21:01:51 +0800

    合入资源文件

---
+++ superwechat/src/cn/ucai/superwechat/net/OnCompleteListener.java
@@ -0,0 +1,12 @@
+package cn.ucai.superwechat.net;
+
+
+import cn.ucai.superwechat.utils.OkHttpUtils;
+
+/**
+ * Created by clawpo on 2017/1/11.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+
+}
