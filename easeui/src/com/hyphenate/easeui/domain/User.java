38a256aa872f553b6c34161b88225396e7bf5b68
zhaoran <2680131355@qq.com>
2017-02-17 16:11:59 +0800

    聊天界面显示头像和昵称

--- easeui/src/com/hyphenate/easeui/domain/User.java
+++ easeui/src/com/hyphenate/easeui/domain/User.java
@@ -79,7 +79,7 @@ public class User implements Serializable {
 	}
 
 	public String getMAvatarLastUpdateTime() {   //  长类型
-		return mavatarLastUpdateTime == null ? String.valueOf(System.currentTimeMillis()) : mavatarLastUpdateTime;
+		return mavatarLastUpdateTime == null ? "" : mavatarLastUpdateTime;
 	}
 
 	public void setMAvatarLastUpdateTime(String mavatarLastUpdateTime) {
